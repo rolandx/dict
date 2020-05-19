f734f0d1ff426d88cdf9dcd88b4a3f1831cf0fa1
rolandX <rat1229@gmail.com>
2020-05-19 10:25:03 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysql_connect('localhost','dict','dict2u4u')
+$connect = mysql_connect('localhost','xxxxx','xxxxxx')
 or die('could not connect:' . mysql_error());
 $db='dict';
 mysql_select_db($db) or die('could not select database ('.$db.') because of :'. mysql_error());
